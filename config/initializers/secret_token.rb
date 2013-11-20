32e5cded17ae2fd898b18bf24e17321ab4196ed6
Thunderbones <dmon88@live.ca>
2013-11-20 13:16:06 -0600

    Initializing Commit for the ecomm project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommProject::Application.config.secret_token = 'd24faa9010d3178fdf8ca20bda2d00a6cd5a9576155ace3b4da13166d0a1b27d569540bfc779c26ee08b6629db8896eaea2fe78d5179b838fc726bfd6312ba92'
